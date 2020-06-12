86e60d3cdde71131d153f3167ec339977ffff4ac
Jacinta Calixte <jacinta246@yahoo.com>
2020-06-12 23:53:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,7 +15,8 @@ def using_flatten (array)
 end
 
 def using_delete (array,string)
-  array.delete("string")
+  array.delete(string)
+end
 
 def using_delete_at (array,integer)
   array.delete_at(1)
